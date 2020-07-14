a9cd8346381ed9fc16d62977e23caa801378ece8
Victor Samson <victorsamson1168@gmail.com>
2020-07-15 00:47:27 +0530

    bootstrap/reactstrap set up

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar, NavbarBrand, Alert, Badge} from 'reactstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+          <Alert className='alert-danger'>hello</Alert>
+        </div>
+      </Navbar>
+      <Badge className="bdgp">hey whats up</Badge>
+      
     </div>
   );
 }
